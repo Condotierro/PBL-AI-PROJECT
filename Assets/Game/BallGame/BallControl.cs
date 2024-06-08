89631a02bf7018e2087cd0c9dafa931d470ba00b
247031 Dawid Maciejewski <davidosx09@gmail.com>
2024-06-08 20:29:38 +0200

    Clean Ups and small tweaks

--- Assets/Game/BallGame/BallControl.cs
+++ Assets/Game/BallGame/BallControl.cs
@@ -5,12 +5,63 @@ using UnityEngine;
 public class BallControl : MonoBehaviour
 {
     public float speed = 5f;
+    int x = 0;
+    int y = 0;
     void Update()
     {
-        Vector3 mouseScreenPosition = Input.mousePosition;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveUp();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveDown();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveRight();
+        }
+        /*Vector3 mouseScreenPosition = Input.mousePosition;
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
         mouseWorldPosition.z = 0;
         Vector3 direction = (mouseWorldPosition - transform.position).normalized;
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position += direction * speed * Time.deltaTime;*/
+    }
+
+    public void SetVisual()
+    {
+        Vector3 v = new Vector3();
+        v.z = 0;
+        v.x = x + 0.5f;
+        v.y = y + 0.5f;
+        transform.position = v;
+    }
+
+    public void MoveLeft()
+    {
+        x--;
+        SetVisual();
+    }
+
+    public void MoveRight()
+    {
+        x++;
+        SetVisual();
+    }
+
+    public void MoveUp()
+    {
+        y++;
+        SetVisual();
+    }
+
+    public void MoveDown()
+    {
+        y--;
+        SetVisual();
     }
 }
